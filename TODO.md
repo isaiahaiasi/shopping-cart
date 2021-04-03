7a56bd657941f26bb6d410e014518caf35141ee7
isaiahaiasi <isaiah.althouse@gmail.com>
2021-04-03 00:35:56 -0700

    Add basic routing

--- TODO.md
+++ TODO.md
@@ -22,3 +22,9 @@
 - Subroutes that use routeparams for individual product pages.
 - A shopping cart context hook that lets you add items to the cart, and get info on what's currently in the cart
 - A dataset of product mocks, containing a link to an image, a product title, a price, a product description, and possibly other information like rating and technical details...
+
+## Other considerations:
+
+- Not yet sure how I want to handle styles. I am really, really sick of trying to write all of my css from scratch. So far, I've resisted using a css framework, but at the very least I should start experimenting with styled-components or something like that...
+- Not sure how to handle images...
+- Not sure what theme I want the shop to have yet. I kind of don't want to go super generic, but I don't think I'm interested in goofy either.
--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
+// TODO:
+// - shopping cart context. State needs iterable of [item id, quantity].
+// (I don't *think* I need any other info in state, since if I have item id,
+//  I can just query for its price, etc)
+// - main nav switch: home, cart, route
+// - routes, including item param route?
+// - (I think that's it, for the main app component)
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+
+const CartContext = React.createContext({
+  /* itemId: quantity, */
+});
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <ul className="nav">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart">Cart</NavLink>
+          </li>
+        </ul>
+
+        <CartContext.Provider>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/shop">
+              <Shop />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+          </Switch>
+        </CartContext.Provider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
